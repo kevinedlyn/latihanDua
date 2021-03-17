598dcd77acf567db507f302dc91601554b5be81b
Kevin Edlyn <c14180208@john.petra.ac.id>
2021-03-17 09:21:41 +0700

    C14180208

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  randisTrue :Boolean = null;
+  randomAngka : number;
+  userNum : number;
+  showRandomisTrue : Boolean;
+  constructor() {
+    this.randomAngka = Math.floor(Math.random() * (100 - 1) + 1);
+  }
 
+  cekRandom()
+  {
+    if(this.randomAngka == this.userNum)
+    {
+      this.randisTrue = true;
+      this.randomAngka = Math.floor(Math.random() * (100 - 1) + 1);
+    }
+    else{
+      this.randisTrue = false;
+    }
+  }
+
+  showRandom()
+  {
+    this.showRandomisTrue = true;
+  }
+
+  unshowRandom()
+  {
+    this.showRandomisTrue = false;
+  }
 }
